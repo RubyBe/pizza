00d78993ef49bb9a8653fdb52d4ad7ad6f3c5713
RubyBe <sidbenavente@hotmail.com>
2016-07-01 09:14:43 -0700

    add constructors for customer, order, and pizza objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-console.log("Hello!");
+function Customer() {
+  console.log("I am a Customer");
+}
+
+function Order() {
+  console.log("I am an Order");
+}
+
+function Pizza() {
+  console.log("I am a Pizza");
+}
+
+myCustomer = new Customer();
+myOrder = new Order();
+myPizza = new Pizza();
