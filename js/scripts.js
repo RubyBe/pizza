662383278f1a324f282c4fb741ee6c4495965dc5
RubyBe <sidbenavente@hotmail.com>
2016-07-01 09:43:46 -0700

    add properties to the customer, order, and pizza objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,29 @@
-function Customer() {
-  console.log("I am a Customer");
+function Customer(name, address, telephone) {
+  this.customerID = 1;
+  this.customerName = name;
+  this.customerAddress = address;
+  this.customerTelephone = telephone;
+  console.log("I am " + this.customerName + ", customer #" + this.customerID + " - " + this.customerAddress + " " + this.customerTelephone);
 }
 
-function Order() {
-  console.log("I am an Order");
+function Order(customerid, selection, type) {
+  this.orderCustomerID = customerid;
+  this.orderSelection = selection;
+  this.orderType = type;
+  this.orderPrice = 0;
+  this.orderDateTime = Date();
+  this.orderTotalPrice = 0;
+  console.log("I am a " + this.orderType + " order for customer #" + this.orderCustomerID + " for a " + this.orderSelection + " of the type: " + this.orderType + ", ordered at " + this.orderDateTime);
 }
 
-function Pizza() {
-  console.log("I am a Pizza");
+function Pizza(size, dough, sauce, toppings) {
+  this.pizzaSize = size;
+  this.pizzaDough = dough;
+  this.pizzaSauce = sauce;
+  this.pizzaToppings = toppings;
+  console.log("I am a " + this.pizzaSize + " pizza on a " + this.pizzaDough + " crust with " + this.pizzaSauce + " sauce and the following toppings: " + this.pizzaToppings);
 }
 
-myCustomer = new Customer();
-myOrder = new Order();
-myPizza = new Pizza();
+var myCustomer = new Customer("Dr. Gonzo", "2000 2nd Avenue, # 5, Seattle", "206-555-1212");
+var myOrder = new Order(1, "Pizza", "Delivery");
+var myPizza = new Pizza("Small", "Whole Wheat", "Garlic", ["Mushrooms", "Sausage", "Green Pepper", "Onion", "Mozzarella"]);
